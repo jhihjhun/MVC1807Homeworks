cbd53326a775a084a30d0424c76d8c505e72c115
周志衠 <jhihjhun@gmail.com>
2018-07-14 02:58:47 +0800

    使用模型驗證實作新增「客戶聯絡人」時，同一個客戶下的聯絡人，其 Email 不能重複

--- WebApp1/ViewModels/客戶聯絡人/CreateViewModel.cs
+++ WebApp1/ViewModels/客戶聯絡人/CreateViewModel.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using WebApp1.Helpers.ValidationAttributes;
 
 namespace WebApp1.ViewModels.客戶聯絡人
 {
-    public class CreateViewModel
+    public class CreateViewModel : IValidatableObject
     {
         [Required]
         public int 客戶Id { get; set; }
@@ -25,5 +27,22 @@ namespace WebApp1.ViewModels.客戶聯絡人
 
         [StringLength(50, ErrorMessage = "欄位長度不得大於 50 個字元")]
         public string 電話 { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            bool emailExist;
+
+            using (Models.客戶資料Entities db = new Models.客戶資料Entities())
+            {
+                var customerContact = db.客戶聯絡人.Where(x => x.客戶Id == 客戶Id && x.刪除 == false).ToList();
+
+                emailExist = customerContact.Any(x => x.Email == Email);
+            }
+
+            if (emailExist)
+            {
+                yield return new ValidationResult("同一個客戶Email不得重複.", new[] { "Email" });
+            }
+        }
     }
 }
