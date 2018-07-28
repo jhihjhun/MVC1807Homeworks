20060b45ae5d64048a1d13a2d7c867804109bdd1
周志衠 <jhihjhun@gmail.com>
2018-07-28 19:40:32 +0800

    * 自訂一個動作過濾器，可以記錄每個 Action 與 ActionResult 執行的時間，資料可以輸出到 Debug 輸出視窗

--- WebApp1/Controllers/AbsBaseController.cs
+++ WebApp1/Controllers/AbsBaseController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApp1.Helpers.ActionFilters;
 
 namespace WebApp1.Controllers
 {
+    [ActionTimeLogFilter]
     public abstract class AbsBaseController : Controller
     {
         protected Models.客戶資料Entities _db;
---
+++ WebApp1/Helpers/ActionFilters/ActionTimeLogFilter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp1.Helpers.ActionFilters
+{
+    public class ActionTimeLogFilter : ActionFilterAttribute
+    {
+        private Stopwatch _stopWatch;
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string actionName = filterContext.ActionDescriptor.ActionName;
+            string controllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
+
+            _stopWatch = new Stopwatch();
+            _stopWatch.Start();
+            Debug.WriteLine($"Controller = {controllerName},  Action = {actionName}");
+            Debug.WriteLine($"OnActionExecuting : {DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")}");
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            _stopWatch.Stop();
+            Debug.WriteLine($"OnResultExecuting : {DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")}");
+            Debug.WriteLine($"Elapsed = {_stopWatch.Elapsed}");
+        }
+    }
+}
